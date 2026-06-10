1464f0e1be150bff598d11ff2a8b872e2ca9b223
TheDemonLord333 <thedemonlord333@gmail.com>
2026-06-10 12:57:36 +0200

    Initial Commit

---
+++ DemonicMusicDashboard/DemonicMusicDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemonicMusicDashboardApp.swift
+//  DemonicMusicDashboard
+//
+//  Created by David Martens on 10.06.26.
+//
+
+import SwiftUI
+
+@main
+struct DemonicMusicDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
