8686aa7c8d66ba8dabbda14b7c8c1693bc6c3516
TheDemonLord333 <thedemonlord333@gmail.com>
2026-07-18 18:13:34 +0200

    Initial Commit

---
+++ DemonicImageGen/DemonicImageGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemonicImageGenApp.swift
+//  DemonicImageGen
+//
+//  Created by David Martens on 18.07.26.
+//
+
+import SwiftUI
+
+@main
+struct DemonicImageGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
